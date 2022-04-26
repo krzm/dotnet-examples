8c6fa9e2f4ee7393b9d32d08cae382bc02d536a4
krzm <kmazanek@gmail.com>
2022-04-26 18:06:58 +0200

    Test AppendFormat params

--- Net.Tests/StringBuilderTests.cs
+++ Net.Tests/StringBuilderTests.cs
@@ -7,7 +7,7 @@ public class StringBuilderTests
 {
 	[Theory]
 	[InlineData("print number: {0}", 10, "print number: 10")]
-	public void IsNullOrWhiteSpace_logic(
+	public void TestAppendFormat(
         string format
         , int number
         , string expected)
@@ -15,6 +15,20 @@ public class StringBuilderTests
         var sut = new StringBuilder();
 
         var acctual = sut.AppendFormat(format, number).ToString();
+		
+        Assert.Equal(expected, acctual);
+	}
+
+    [Theory]
+	[InlineData("print number: {0}, {1}, {2}", new object[] { 10, 20, 30 }, "print number: 10, 20, 30")]
+	public void TestAppendFormatParams(
+        string format
+        , object[] numbers
+        , string expected)
+	{
+        var sut = new StringBuilder();
+
+        var acctual = sut.AppendFormat(format, numbers).ToString();
 		
         Assert.Equal(expected, acctual);
 	}
